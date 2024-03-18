3e5843a14025f665e95d3fdd5a3233d5971759da
Wageh Ayed <Wageh.ayed@gmail.com>
2024-03-18 23:40:25 +0300

    ELK With .net core and File beat , JDBC

---
+++ ELK-pipline/SerilogTestApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.File;
+namespace  SerilogTestApp
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            
+            // configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("./../logs/logs.log")
+                .CreateLogger();
+
+            try
+            {
+                
+                
+                
+                Log.Information("Starting app ...");
+                
+                int x=5;
+                int y=8;
+                var z= (x+y)/(x-5);
+                //CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex){
+                Log.Fatal(ex,"Application Terminate Unexpected");
+            }
+            finally
+            {
+                Log.Information("Finishing app ...");
+                Log.CloseAndFlush();
+            }
+
+        }
+    }
+    
+}
